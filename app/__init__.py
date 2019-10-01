d6a83ee6ef3339fd9c73dec5eca41e943a56542b
TorryTheMaltese <48584054+TorryTheMaltese@users.noreply.github.com>
2019-10-01 15:34:26 +0900

    test

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+from app import errors
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def hello():
-    return '<h1>Hello World!</h1>'
+    if request.method == 'POST':
+        name = 'Stranger'
+    elif request.method == 'GET':
+        name = request.args.get('name')
+    return render_template('index.html', name=name)
